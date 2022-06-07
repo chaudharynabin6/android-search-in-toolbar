3e25a3685aa107549aacfe18ff00a54c23126889
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-06-07 21:46:37 +0545

    simple search view : completed

--- app/src/main/java/com/chaudharynabin6/searchview/MainActivity.kt
+++ app/src/main/java/com/chaudharynabin6/searchview/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.chaudharynabin6.searchview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 
-class MainActivity : AppCompatActivity() {
+import com.chaudharynabin6.searchview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(
+            layoutInflater
+        )
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        menuInflater.inflate(R.menu.search_menu,menu)
+
+        val searchMenu = menu.findItem(R.id.search)
+        val searchView  = searchMenu.actionView as SearchView
+        searchView.apply {
+
+            queryHint = "Type here to search"
+            setOnQueryTextListener(this@MainActivity)
+
+
+        }
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+
+        Log.e("onSubmit ...",query.toString())
+        return false
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e("onChange ...",newText.toString())
+
+        return false
     }
 }
